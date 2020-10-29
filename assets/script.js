39827aa96d8a2ae98a60e55e8a8bb998a8d6d3c8
Peter Phenow <peter.phenow@gmail.com>
2020-10-28 22:25:01 -0500

    saving user input to local storage now works

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,36 @@ let currentDay = $("#currentDay");
 let currentDate = moment().format("dddd, MMM Do, YYYY");
 currentDay.text(currentDate);
 
-//get save button to save to local storage
+//sets variable to an empty object for use later
+let userInput = {};
+
+//allows save button to save to local storage
 let saveBtn = $(".saveBtn");
 saveBtn.on("click", function () {
-  console.log("save worked!");
-});
+  //take all user inputs and sets to variables
+  let nine = $("#nine").val();
+  let ten = $("#ten").val();
+  let eleven = $("#eleven").val();
+  let twelve = $("#twelve").val();
+  let one = $("#one").val();
+  let two = $("#two").val();
+  let three = $("#three").val();
+  let four = $("#four").val();
+  let five = $("#five").val();
 
-// allow user to input notes for each hour
-// let userInputEl = $(".description");
-// userInputEl.on("click", function () {
-//   console.log("click worked!");
-// });
+  //creates an object to save to local storage
+  userInput = {
+    nineAM: nine,
+    tenAM: ten,
+    elevenAM: eleven,
+    twelvePM: twelve,
+    onePM: one,
+    twoPM: two,
+    threePM: three,
+    fourPM: four,
+    fivePM: five,
+  };
+
+  //saves above object to local storage using stringify
+  localStorage.setItem("schedule", JSON.stringify(userInput));
+});
